7b8103d1518751deccfe86e7bf067ca69dac3005
AnaClaraazz <anaclaraazzevedo2022@gmail.com>
2022-09-06 14:28:36 -0300

    Primeira atividade em JS

---
+++ index.js
@@ -0,0 +1,10 @@
+let nome, idade, filhos, filmefavorito, gostomusical, sonho
+
+nome = prompt("Escreva seu nome: ");
+idade = prompt("Qual sua idade: ");
+gostomusical = prompt("Qual seu gosto musical: ");
+filmefavorito = prompt("Qual seu filme preferido: ");
+filhos = prompt("Possui filhos: ");
+sonho = prompt("Um sonho: ");
+alert("Seu nome é " + nome + "\nSua idade é: " + idade + "\nSeu gosto musical é: " + gostomusical + "\nSeu filme preferido é: " + filmefavorito + "\nSe tem filhos: " + filhos + "\nSeu sonho é: " + sonho);
+
